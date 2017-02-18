8e27479717399ef3cc8540536bfdd1050895c405
Marcus Kopp <marcus.kopp86@gmail.com>
2017-02-18 16:25:48 +0100

    Array prototype

--- src/main/TweetGen.java
+++ src/main/TweetGen.java
@@ -3,10 +3,31 @@
  */
 package main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @author Marcus Kopp
- * Feb 18, 2017
+ * @author Marcus Kopp Feb 18, 2017
  */
 public class TweetGen {
+	public static void main(String[] args) throws IOException {
+		//File bewertungen = new File("ressources/bewertungen.txt");
+		File headlines = new File("ressources/headlines.txt");
+		FileReader fr = new FileReader(headlines);
+		BufferedReader br = new BufferedReader(fr);
+		List<String> headlineArray = new ArrayList<String>();
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			headlineArray.add(line);
+		}
+		for (String string : headlineArray) {
+			System.out.println(string);
+		}
+		br.close();
 
+	}
 }
