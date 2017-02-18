019600e06fe79d4dffefcba13acca0a2e55dcf2e
sina spaeth <s.spaeth@student.htw-berlin.de>
2017-02-18 19:06:34 +0100

    mit gui

--- src/main/TweetGen.java
+++ src/main/TweetGen.java
@@ -47,12 +47,13 @@ public class TweetGen extends Application {
 		}
 	}
 	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/main/main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/main/tweet.fxml"));
 			Scene scene = new Scene(root,500,500);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
